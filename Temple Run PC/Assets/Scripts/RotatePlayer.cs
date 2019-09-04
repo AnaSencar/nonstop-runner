f196dd342206f480822b091758c7c97598f70130
AnaSencar <54727715+AnaSencar@users.noreply.github.com>
2019-09-05 00:51:39 +0200

    Tryout for movement rotation

---
+++ Temple Run PC/Assets/Scripts/RotatePlayer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class RotatePlayer : MonoBehaviour
+{
+    private bool isPlayerInsideTrigger = false;
+    private bool isLeftPlatform = false;
+    private bool isRightPlatform = false;
+    private ThirdPersonCharacter thirdPersonCharacter;
+
+    public bool IsPlayerInsideTrigger
+    {
+        set
+        {
+            isPlayerInsideTrigger = value;
+        }
+    }
+
+    public bool IsLeftPlatform
+    {
+        set
+        {
+            isLeftPlatform = value;
+        }
+    }
+
+    public bool IsRightPlatform
+    {
+        set
+        {
+            isRightPlatform = value;
+        }
+    }
+
+    private void Awake()
+    {
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlayerInsideTrigger)
+        {
+            if(Input.GetKeyDown(KeyCode.Q) && isLeftPlatform)
+            {
+                transform.Rotate(transform.rotation.x, transform.rotation.y - 90f, transform.rotation.z);
+                Vector3 movement = transform.forward * 2f;
+                thirdPersonCharacter.Move(movement, false, false);
+            }
+            else if (Input.GetKeyDown(KeyCode.E) && isRightPlatform)
+            {
+                transform.Rotate(transform.rotation.x, transform.rotation.y + 90f, transform.rotation.z);
+                Vector3 movement = transform.forward * 2f;
+                thirdPersonCharacter.Move(movement, false, false);
+            }
+        }
+    }
+}
---
+++ Temple Run PC/Assets/Scripts/RotationTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTrigger : MonoBehaviour
+{
+    [SerializeField] bool isLeftPlatform = false;
+    [SerializeField] bool isRightPlatform = false;
+    private RotatePlayer rotatePlayer;
+
+    private void Awake()
+    {
+        rotatePlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<RotatePlayer>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rotatePlayer.IsPlayerInsideTrigger = true;
+        SetPlatforms();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        rotatePlayer.IsPlayerInsideTrigger = false;
+        SetPlatforms();
+    }
+
+    private void SetPlatforms()
+    {
+        rotatePlayer.IsRightPlatform = isRightPlatform;
+        rotatePlayer.IsLeftPlatform = isLeftPlatform;
+    }
+
+}
