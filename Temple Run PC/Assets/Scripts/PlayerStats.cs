45f49d31ad4559bc672715248e4079aefa714b5e
AnaSencar <54727715+AnaSencar@users.noreply.github.com>
2019-09-06 22:30:23 +0200

    Added coin collecting. Having bug cause platforms cloning.

---
+++ Temple Run PC/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private int coinsCollected = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+            coinsCollected += 1;
+            Destroy(other.gameObject);
+            Debug.Log(coinsCollected);
+        }
+    }
+}
