ec930ac5146e452859929007837cb429d6451dd9
AnaSencar <54727715+AnaSencar@users.noreply.github.com>
2019-09-06 21:16:13 +0200

    Added obstacles.

---
+++ Temple Run PC/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] obstaclesPrefabs;
+    [SerializeField] Transform[] obstacleSpawnPoints;
+
+    void Start()
+    {
+        obstacleSpawnPoints = GetComponentsInChildren<Transform>();
+        SpawnObstacle();
+    }
+
+    private void SpawnObstacle()
+    {
+        for (int i = 1; i < obstacleSpawnPoints.Length; i++)
+        {
+            if (ShouldSpawnOnThisSpawnPoint())
+            {
+                int whichObstacleToSpawn = Random.Range(0, obstaclesPrefabs.Length);
+                Instantiate(obstaclesPrefabs[whichObstacleToSpawn],obstacleSpawnPoints[i]);
+            }
+        }
+    }
+
+    private bool ShouldSpawnOnThisSpawnPoint()
+    {
+        int shouldSpawnRandomer = Random.Range(0, 100);
+        return (shouldSpawnRandomer >= 50 ? true : false);
+    }
+
+}
