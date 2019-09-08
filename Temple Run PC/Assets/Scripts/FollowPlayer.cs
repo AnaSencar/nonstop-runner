3f5f3d0a38410782d0dd9f793284f1040ff84e9d
AnaSencar <54727715+AnaSencar@users.noreply.github.com>
2019-09-08 02:44:43 +0200

    Added platform following player for collision.

---
+++ Temple Run PC/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void Update()
+    {
+        Vector3 newPositionOFLava = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        transform.position = newPositionOFLava;
+    }
+
+}
